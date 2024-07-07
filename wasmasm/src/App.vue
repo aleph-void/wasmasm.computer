066dd7c40063cfe7dbe7b9918affd04bb0cb0494
nstarke <starke.nicholas@gmail.com>
2024-07-07 17:04:54 -0500

    some styling

--- wasmasm/src/App.vue
+++ wasmasm/src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <MainView />
+  <div class="container-fluid">
+    <div class="container">
+      <h1>WASMASM</h1>
+      <p>WebAssembly-based Assembler</p>
+      <p>
+        <a href="https://www.keystone-engine.org/">Built with Keystone</a>
+      </p>
+      <p>Written by Nicholas Starke: <a href="https://starkeblog.com/">StarkeBlog.com</a></p>
+    </div>
+    <MainView />
+  </div>
 </template>
 
 <script>
@@ -18,6 +28,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
 }
 </style>
--- wasmasm/src/components/MainView.vue
+++ wasmasm/src/components/MainView.vue
@@ -1,52 +1,61 @@
 <template>
-  <div>
-    <div>
-      <label for="input">Input</label>
-      <textarea id="input" v-model="input"></textarea>
+  <form onsubmit="return false;">
+    <div class="container">
+      <fieldset>
+        <div>
+          <span style="color: red;"> {{ errorMessage }}</span>
+        </div>
+        <div>
+          <label for="input" class="form-label">Input</label>
+          <textarea id="input" class="form-control" v-model="input"></textarea>
+        </div>
+        <div>
+          <label for="selectedISA" class="form-label">Instruction Set Architecture</label>
+          <select class="form-select" id="selectedISA" v-model="selectedISA">
+            <option selected disabled>Instruction Set Architecture</option>
+            <option value="ARM">ARM</option>
+            <option value="x86">x86</option>
+            <option value="MIPS">MIPS</option>
+            <option value="PPC">PPC</option>
+            <option value="SPARC">SPARC</option>
+          </select>
+        </div>
+        <div>
+          <label for="selectedWordSize" class="form-label">Word Size</label>
+          <select class="form-select" id="selectedWordSize" v-model="selectedWordSize" v-on:change="selectedWordSizeChanged">
+            <option selected disabled>Word Size</option>
+            <option value="16">16-bit</option>
+            <option value="32">32-bit</option>
+            <option value="64">64-bit</option>
+          </select>
+        </div>
+        <div>
+          <label for="selectedEndianness" class="form-label">Endianness</label>
+          <select class="form-select" id="selectedEndianness" v-model="selectedEndianness" v-on:change="selectedEndiannessChanged">
+            <option selected disabled>Endianness</option>
+            <option value="big">Big</option>
+            <option value="small">Small</option>
+          </select>
+        </div>
+        <!-- <div>
+          <label for="selectedExtra">Extras</label>
+          <select v-model="selectedExtra" v-on:change="selectedExtraChanged">
+            <option selected disabled>Extras</option>
+            <option value="ARMv8">ARMv8</option>
+          </select>
+        </div> -->
+        <div>
+          <br />
+          <button class="btn btn-primary" v-on:click="buttonClicked">Assemble</button>
+        </div>
+        <div>
+          <br />
+          <label for="output" class="form-label">Output</label>
+          <textarea class="form-select" disabled id="output" v-model="output"></textarea>
+        </div>
+      </fieldset>
     </div>
-    <div>
-      <label for="selectedISA">Instruction Set Architecture</label>
-      <select id="selectedISA" v-model="selectedISA">
-        <option selected disabled>Instruction Set Architecture</option>
-        <option value="ARM">ARM</option>
-        <option value="x86">x86</option>
-        <option value="MIPS">MIPS</option>
-        <option value="PPC">PPC</option>
-        <option value="SPARC">SPARC</option>
-      </select>
-    </div>
-    <div>
-      <label for="selectedWordSize">Word Size</label>
-      <select id="selectedWordSize" v-model="selectedWordSize" v-on:change="selectedWordSizeChanged">
-        <option selected disabled>Word Size</option>
-        <option value="16">16-bit</option>
-        <option value="32">32-bit</option>
-        <option value="64">64-bit</option>
-      </select>
-    </div>
-    <div>
-      <label for="selectedEndianness">Endianness</label>
-      <select id="selectedEndianness" v-model="selectedEndianness" v-on:change="selectedEndiannessChanged">
-        <option selected disabled>Endianness</option>
-        <option value="big">Big</option>
-        <option value="small">Small</option>
-      </select>
-    </div>
-    <div>
-      <label for="selectedExtra">Extras</label>
-      <select v-model="selectedExtra" v-on:change="selectedExtraChanged">
-        <option selected disabled>Extras</option>
-        <option value="ARMv8">ARMv8</option>
-      </select>
-    </div>
-    <div>
-      <button v-on:click="buttonClicked">Assemble</button>
-    </div>
-    <div>
-      <label for="output">Output</label>
-      <textarea disabled id="output" v-model="output"></textarea>
-    </div>
-  </div>
+  </form>
 </template>
 
 <script>
@@ -61,7 +70,8 @@ export default {
       selectedWordSize: "",
       selectedEndianness: "",
       selectedExtra: "",
-      assembler: null
+      assembler: null,
+      errorMessage: ""
     }
   },
   async created() {
@@ -69,10 +79,11 @@ export default {
     
   },
   async mounted() {
-
+    this.errorMessage = "";
   },
   methods: {
     buttonClicked() {
+      this.errorMessage = "";
       const inputBuffer = this.assembler._malloc(this.input.length + 1);
       this.assembler.stringToUTF8(this.input, inputBuffer, this.input.length + 1);
       const isaBuffer = this.assembler._malloc(this.selectedISA.length + 1);
@@ -82,8 +93,14 @@ export default {
       this.assembler._assemble(inputBuffer, this.input.length, isaBuffer, this.selectedEndianness === 'big' ? 1 : 2, parseInt(this.selectedWordSize), outputBuffer);
       this.assembler._free(inputBuffer);
       this.assembler._free(isaBuffer);
-      this.output = this.assembler.UTF8ToString(outputBuffer);
+      const out = this.assembler.UTF8ToString(outputBuffer);
+      if (out){
+        this.output = out;
+      } else {
+        this.errorMessage = "Problem with disassembly. Check settings and try again";
+      }
       this.assembler._free(outputBuffer);
+      return false;
     }
   }
 }   
@@ -91,13 +108,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 
 </style>
